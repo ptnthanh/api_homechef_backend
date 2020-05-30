e41021bd5e1553f4566ca395c35912b4792b99dd
Phan Quân <qmp23@drexel.edu>
2020-05-29 23:59:14 -0400

    First commit

---
+++ app/routes/usersRoute.js
@@ -0,0 +1,14 @@
+  
+import express from 'express';
+
+import { createUser, siginUser, searchFirstnameOrLastname } from '../controllers/usersController';
+
+const router = express.Router();
+
+// users Routes
+
+router.post('/auth/signup', createUser);
+router.post('/auth/signin', siginUser);
+router.get('/users/first_name', searchFirstnameOrLastname);
+
+export default router;
